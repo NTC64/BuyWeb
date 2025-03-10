d5c2333d667192bd35fb7bb6f526a5ebc80c739e
NTC64 <nguyenthanhchung.06042002@gmail.com>
2025-03-10 23:27:05 +0700

    first commit

---
+++ BuyWebFrontend/src/components/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+    <div>
+        <h1>Home</h1>
+        <button @click="count++">Click me</button>
+        <p>Count is {{ count }}</p>
+    </div>
+    <div>
+        <h1>Home</h1>
+        <button @click="count++">Click me</button>
+        <p>Count is {{ count }}</p>
+    </div>
+    <div>
+        <h1>Home</h1>
+        <button @click="count++">Click me</button>
+        <p>Count is {{ count }}</p>
+    </div>
+</template>
